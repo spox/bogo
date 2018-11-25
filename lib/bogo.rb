d2b89d6da280052f662f3ed340ce0b6d8a64d4f9
Chris Roberts <code@chrisroberts.org>
2018-11-25 06:27:36 -0800

    Add logger utility

--- lib/bogo.rb
+++ lib/bogo.rb
@@ -1,16 +1,17 @@
-require 'bogo/version'
+require "bogo/version"
 
 module Bogo
-  autoload :AnimalStrings, 'bogo/animal_strings'
-  autoload :Constants, 'bogo/constants'
-  autoload :EphemeralFile, 'bogo/ephemeral_file'
-  autoload :Lazy, 'bogo/lazy'
-  autoload :Memoization, 'bogo/memoization'
-  autoload :PriorityQueue, 'bogo/priority_queue'
-  autoload :Retry, 'bogo/retry'
-  autoload :Smash, 'bogo/smash'
-  autoload :Utility, 'bogo/utility'
+  autoload :AnimalStrings, "bogo/animal_strings"
+  autoload :Constants, "bogo/constants"
+  autoload :EphemeralFile, "bogo/ephemeral_file"
+  autoload :Lazy, "bogo/lazy"
+  autoload :Logger, "bogo/logger"
+  autoload :Memoization, "bogo/memoization"
+  autoload :PriorityQueue, "bogo/priority_queue"
+  autoload :Retry, "bogo/retry"
+  autoload :Smash, "bogo/smash"
+  autoload :Utility, "bogo/utility"
 end
 
 # Always load smash
-require 'bogo/smash'
+require "bogo/smash"
---
+++ lib/bogo/logger.rb
@@ -0,0 +1,84 @@
+require "concurrent"
+require "logger"
+
+module Bogo
+  class Logger
+    module Helpers
+      module InstanceMethods
+        def logger
+          self.class.logger
+        end
+      end
+
+      module ClassMethods
+        def logger_name(name)
+          @_logger = logger.named(name.to_s)
+        end
+
+        def logger
+          if !@_logger
+            base = Utility.snake(self.name.
+              split("::").first.downcase)
+            @_logger = Bogo::Logger.logger.named(base)
+          end
+          @_logger
+        end
+      end
+
+      def self.included(klass)
+        klass.include(InstanceMethods)
+        klass.extend(ClassMethods)
+      end
+    end
+
+    # @return [Logger] default logger
+    def self.logger
+      if !@_logger
+        @_logger = new
+      end
+      @_logger
+    end
+
+    # Create a new ::Logger instance wrapped
+    # to allow threaded interactions
+    def initialize(*logger_args)
+      if logger_args.empty?
+        logger_args = [$stderr]
+      end
+      @base_args = logger_args
+      logger = ::Logger.new(*@base_args)
+      @wrapped_logger = Concurrent::MVar.new(logger)
+    end
+
+    # Create a new logger with the sub-name provided
+    #
+    # @param name [String] sub-name for logger
+    # @return [Logger]
+    def named(name)
+      new_name = self.progname.to_s
+      new_name << "." unless new_name.empty?
+      new_name << name
+      new_logger = Logger.new(*@base_args)
+      [:level, :formatter, :datetime_format].each do |m|
+        new_logger.send("#{m}=", self.send(m))
+      end
+      new_logger.progname = new_name
+      new_logger
+    end
+
+    ::Logger.instance_methods.each do |l_m|
+      next if l_m.to_s.start_with?("_") || l_m.to_s == "object_id"
+      class_eval <<-EOC
+      def #{l_m}(*ma, &mb)
+      wrapped_logger.borrow { |l| l.send(:#{l_m}, *ma, &mb) }
+      end
+      EOC
+    end
+
+    protected
+
+    def wrapped_logger
+      @wrapped_logger
+    end
+  end
+end
