5ebe6193b7a98ee5e594b754bb444b08df97f6f8
Chris Roberts <code@chrisroberts.org>
2022-07-02 17:36:45 -0700

    Add simple flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "bogo dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ ruby_3_1 ]; };
+      });
+}
